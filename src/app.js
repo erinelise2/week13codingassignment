76fd4b6838fcd4a4e215b26195930eddc2cdd52f
Erin Brickman <eelise187@Erins-MBP.attlocal.net>
2023-06-26 19:59:26 -0700

    organized files and folders

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import './app.css'
+import Nav from '../nav'
+import Section from '../section'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Section/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/section.js
@@ -0,0 +1,20 @@
+
+import React, {Component} from 'react'
+
+let date = new Date()
+
+let singleUser = {
+  name: 'Erin',
+  time: date.toDateString(),
+}
+
+export default class Section extends Component {
+  render() {
+    return (
+      <section className="welcomeSection">
+        <h1>Welcome, {singleUser.name}</h1>
+        <p>Todays date is: {singleUser.time}</p>
+      </section>
+    )
+  }
+}
