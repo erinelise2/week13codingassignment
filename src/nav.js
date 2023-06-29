3b5d5da7aeb91815324ebac16e0331fc9e8fc79b
Erin Brickman <eelise187@Erins-MBP.attlocal.net>
2023-06-29 07:17:57 -0700

    commiting new files

--- src/nav.js
+++ src/nav.js
@@ -3,12 +3,20 @@ import React, {Component} from 'react'
 export default class Nav extends Component {
   render() {
     return (
-      <nav className="navBar">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
+      <nav className="navBar navbar-expand-lg navbar-light p-1">
+        <div>
+          <ul>
+            <li>
+              <a className="nav-link" href="" target="_blank" rel="noreferrer">Home</a>
+            </li>
+            <li>
+              <a className="nav-link" href="" target="_blank" rel="noreferrer">About</a>
+            </li>
+            <li>
+              <a className="nav-link" href="" target="_blank" rel="noreferrer">Contact Us</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     )
   }
