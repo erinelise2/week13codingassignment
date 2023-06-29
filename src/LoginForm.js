33a346fd83d800dd4764c966ce6f1381a86d9021
erinelise2 <erinelise2@gmail.com>
2023-06-29 08:07:55 -0700

    testing push

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,32 +1,10 @@
 
 import React, {Component} from 'react'
-import  React, { useState , useEffect } from 'react'
+
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-export const DateTime = () => {
-
-    var [date,setDate] = useState(new Date());
-    
-    useEffect(() => {
-        var timer = setInterval(()=>setDate(new Date()), 1000 )
-        return function cleanup() {
-            clearInterval(timer)
-        }
-    
-    });
-
-    return(
-        <div>
-            <p> Time : {date.toLocaleTimeString()}</p>
-            <p> Date : {date.toLocaleDateString()}</p>
-
-        </div>
-    )
-}
-
-export default DateTime
 
 let singleUser = {
   name: 'Erin',
